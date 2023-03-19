e378b320833a9a6ae6e13ea4ff8d8d9345563eb3
Yaroslav Paroshin <qzarov@mail.ru>
2023-03-19 14:05:16 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import {useEffect} from "react";
+
+function App() {
+    useEffect(() => {
+        tg.ready();
+    }, [])
+    const onClose = () => {
+        tg.close()
+    }
+  return (
+    <div className="App">
+      work
+        <button onClick={onClose}>Закрыть</button>
+    </div>
+  );
+}
+
+export default App;
