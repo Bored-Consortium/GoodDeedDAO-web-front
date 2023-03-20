588293971fa26227573d28f1bc1f17f7404c3de9
Yaroslav Paroshin <qzarov@mail.ru>
2023-03-20 11:45:06 +0300

    Fix test text2

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,8 +9,8 @@ const Header = () => {
         <div className={'header'}>
             <span className={'username'}>
                 Вы авторизованы как: {user?.username}
-                \nНа данный момент в игре "ZovDobra" ассета нет
-                \nComing soon, как говорится
+                <br>На данный момент в игре "ZovDobra" ассета нет</br>
+                <br>Coming soon, как говорится</br>
             </span>
         </div>
     );
