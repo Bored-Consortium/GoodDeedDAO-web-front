6eae1e1055e97ff9d95ae0b922b72ebf65923ede
Yaroslav Paroshin <qzarov@mail.ru>
2023-03-26 16:34:47 +0300

    Cancel previous changes

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -27,7 +27,7 @@ const ProductList = () => {
             queryId,
         }
         console.log("data: ", data);
-        fetch('http://37.9.5.17:8080', {
+        fetch('http://37.9.5.17:8080/web-data', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
